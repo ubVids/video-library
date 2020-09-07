36810e4e1504b4f914a9aaf153359fcc598bac26
sc0rp1on <singhsooraj.baliyan@gmail.com>
2020-09-07 06:15:31 +0530

    New Vids

--- scripts/1/jsonGenerator.py
+++ scripts/1/jsonGenerator.py
@@ -8,22 +8,22 @@ def list_of_files(dirHere,ext):
             files.append(f)
     return (files)
 fileArr = list_of_files('../../dist/1', "m4v")
-distName = "https://cdn.jsdelivr.net/gh/ubVids/video-library@latest/dist/1"
+distName = "https://cdn.jsdelivr.net/gh/ubVids/video-library@latest/dist/1/"
 sourceArray = []
 
 for f in fileArr:
     source = f
     title = f.split('.mp4')[0]
     # thumb = title+'.jpg'
-    sourceArray.append({
-        "source": distName+source,
-        # "thumb": distName+thumb,
-        "title": title
-    })
+    sourceArray.append(distName+source
+        # "source": distName+source,
+        # # "thumb": distName+thumb,
+        # "title": title
+    )
 
 extraFunction = 'function randomNoRepeats(e){let t=e.slice(0);return function(){t.length<1&&(t=e.slice(0));let o=Math.floor(Math.random()*t.length),n=t[o];t.splice(o,1);let r=document.getElementById("video-source"),c=document.getElementById("content_video");return r.setAttribute("src",n.sources[0]),c.setAttribute("poster",n.thumb[0]),n}}let changeVideoSource=randomNoRepeats(sourceArray);changeVideoSource(),changeVideoSource();'
 
 f = open("library.js", "w")
 f.write("let sourceArray = "+ json.dumps(sourceArray))
-f.write("\n"+extraFunction)
+# f.write("\n"+extraFunction)
 f.close()
--- scripts/jsonGenerator.py
+++ scripts/jsonGenerator.py
@@ -15,15 +15,15 @@ for f in fileArr:
     source = f
     title = f.split('.mp4')[0]
     thumb = title+'.jpg'
-    sourceArray.append({
-        "source": distName+source,
-        "thumb": distName+thumb,
-        "title": title
-    })
+    sourceArray.append(distName+source
+        # "source": distName+source
+        # "thumb": distName+thumb,
+        # "title": title
+    )
 
-extraFunction = 'function randomNoRepeats(e){let t=e.slice(0);return function(){t.length<1&&(t=e.slice(0));let o=Math.floor(Math.random()*t.length),n=t[o];t.splice(o,1);let r=document.getElementById("video-source"),c=document.getElementById("content_video");return r.setAttribute("src",n.sources[0]),c.setAttribute("poster",n.thumb[0]),n}}let changeVideoSource=randomNoRepeats(sourceArray);changeVideoSource(),changeVideoSource();'
+# extraFunction = 'function randomNoRepeats(e){let t=e.slice(0);return function(){t.length<1&&(t=e.slice(0));let o=Math.floor(Math.random()*t.length),n=t[o];t.splice(o,1);let r=document.getElementById("video-source"),c=document.getElementById("content_video");return r.setAttribute("src",n.sources[0]),c.setAttribute("poster",n.thumb[0]),n}}let changeVideoSource=randomNoRepeats(sourceArray);changeVideoSource(),changeVideoSource();'
 
 f = open("library.js", "w")
 f.write("let sourceArray = "+ json.dumps(sourceArray))
-f.write("\n"+extraFunction)
+# f.write("\n"+extraFunction)
 f.close()
